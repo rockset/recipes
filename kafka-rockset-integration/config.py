614729f48280d61988793b7eecf1f4b47dea7bd9
Abhishek <abhishek.mulay@velotio.com>
2018-12-21 12:10:54 +0530

    Apply Transformation on Data

--- kafka-rockset-integration/config.py
+++ kafka-rockset-integration/config.py
@@ -1,6 +1,11 @@
-LIMIT_DATA_TILL = '2011-01-01'
-DATA_PATH = './data/data.csv'
+# Data Source
+LIMIT_DATA_TILL = '2010-12-31 23:59:59'  # Ingest data from "2010-01-01 00:00:00" to this timestamp
+DATA_PATH = './data/data.csv'  # Location of dataset
 
 # Kafka Configuration
 KAFKA_TOPIC = 'orders'
 KAFKA_BOOTSTRAP_SERVER = ['localhost:9092']
+
+# Rockset Configuration - # Create API Key - https://console.rockset.com/manage/apikeys
+ROCKSET_API_KEY = ''
+ROCKSET_API_SERVER = 'https://api.rs2.usw2.rockset.com'
--- kafka-rockset-integration/ingest_data_into_kafka.py
+++ kafka-rockset-integration/ingest_data_into_kafka.py
@@ -1,3 +1,5 @@
+"""Ingest a data from file to Kafka"""
+
 import csv
 import json
 
@@ -6,18 +8,18 @@ from kafka import KafkaProducer
 
 from config import DATA_PATH, KAFKA_BOOTSTRAP_SERVER, LIMIT_DATA_TILL, KAFKA_TOPIC
 
-kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
-
-LIMIT_DATA_TILL_EPOCH = int(dateparser.parse(LIMIT_DATA_TILL).timestamp())
-
 
 def read_records_from_file(file_name):
     """
-    Reads `file_name` and returns a iterator over the records
+    Read ``file_name`` and returns a ``Iterator`` over the records
 
-    :param file_name: (str) - File name to read data from
-    :return: `Iterator`
+    Args:
+        file_name (str): File name to read data from
+
+    Returns:
+        Iterator
     """
+    print('Reading data from file {}'.format(file_name))
     with open(file_name, encoding='latin1') as fin:
         reader = csv.DictReader(fin)
         for counter, record in enumerate(reader):
@@ -26,16 +28,18 @@ def read_records_from_file(file_name):
 
 def apply_transformations(record):
     """
-    Optionally perform any data transformations this can also be done using field mapping explained
+    Perform any data transformations this can also be done using field mapping explained
     here https://docs.rockset.com/field-mapping/
 
-    :param record: dict(str, any)
-    :return: None
-    """
-    # In this example we don't need a product description
-    record.pop('Description')
+    Args:
+        record (dict[str, any]:
 
-    record['Country'] = record['Country'].strip().replace(' ', '')
+    Returns:
+        None
+    """
+    record['Quantity'] = int(record['Quantity'])
+    record['UnitPrice'] = float(record['UnitPrice'])
+    record['CustomerID'] = int(record['CustomerID'])
 
     # Rockset supports _event_time in seconds_since_epoch or milliseconds_since_epoch
     record['InvoiceDate'] = int(dateparser.parse(record['InvoiceDate']).timestamp())
@@ -43,21 +47,36 @@ def apply_transformations(record):
 
 def ingest_into_kafka(records):
     """
-    Ingest `records` in Kafka
+    Ingest ``records`` in Kafka
 
-    :param records: Iterator - Iterator over records. Each record is a dict
-    :return: None
+    Args:
+        records (Iterator): Iterator over records. Each record is a dict
+
+    Returns:
+        None
     """
+    kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
+    limit_data_till_epoch = int(dateparser.parse(LIMIT_DATA_TILL).timestamp())
+
+    print('Ingesting records into Kafka. Kafka Server {}, Topic {}'.format(
+        ','.join(KAFKA_BOOTSTRAP_SERVER),
+        KAFKA_TOPIC))
+
+    count = 0
     for record in records:
-        apply_transformations(record)
+        if record['CustomerID']:
+            apply_transformations(record)
 
-        print(json.dumps(record))
+            kafka_producer.send(KAFKA_TOPIC, str.encode(json.dumps(record)))
 
-        # Produce messages asynchronously
-        kafka_producer.send(KAFKA_TOPIC, str.encode(json.dumps(record)))
+            count += 1
+            if count % 100 == 0:
+                print('{} records ingested'.format(count))
 
-        if record['InvoiceDate'] > LIMIT_DATA_TILL_EPOCH:
-            break
+            if record['InvoiceDate'] > limit_data_till_epoch:
+                break
+
+    print('{} records ingested'.format(count))
 
 
 if __name__ == '__main__':
