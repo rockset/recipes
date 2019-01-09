660a1f740a84559310ec57a82f57d1ed8f86cabd
abhishek-velotio <abhishek.mulay@velotio.com>
2019-01-09 18:04:26 +0530

    Simulate e-commerce order generation

--- kafka-rockset-integration/config.py
+++ kafka-rockset-integration/config.py
@@ -1,7 +1,3 @@
-# Data Source
-LIMIT_DATA_TILL = '2015-12-31 23:59:59'  # Ingest data from "2010-01-01 00:00:00" to this timestamp
-DATA_PATH = './data/data.csv'  # Location of dataset
-
 # Kafka Configuration
 KAFKA_TOPIC = 'orders'
 KAFKA_BOOTSTRAP_SERVER = ['localhost:9092']
@@ -9,3 +5,19 @@ KAFKA_BOOTSTRAP_SERVER = ['localhost:9092']
 # Rockset Configuration - # Create API Key - https://console.rockset.com/manage/apikeys
 ROCKSET_API_KEY = ''
 ROCKSET_API_SERVER = 'https://api.rs2.usw2.rockset.com'
+
+# Simulating 500 customers
+MIN_CUSTOMER_ID = 10000
+MAX_CUSTOMER_ID = 10500
+
+# Control no. of products to include per order
+MIN_PRODUCTS_PER_ORDER = 1
+MAX_PRODUCTS_PER_ORDER = 10
+
+# Control no. of products to include per order
+MIN_QUANTITY_PER_PRODUCT = 1
+MAX_QUANTITY_PER_PRODUCT = 3
+
+# Control no. of orders per second
+MIN_ORDERS_PER_SEC = 1
+MAX_ORDERS_PER_SEC = 300
---
+++ kafka-rockset-integration/write_data_into_kafka.py
@@ -0,0 +1,104 @@
+"""Write a data into Kafka"""
+
+import datetime
+import json
+import random
+import time
+
+from kafka import KafkaProducer
+
+from config import *
+
+START_INVOICE_ID_FROM = 0
+
+# Generate orders across these countries
+# Duplicates are intentional to simulate non-uniform data
+COUNTRIES = ['United States', 'United States', 'United States', 'China', 'China', 'India', 'India',
+             'United Kingdom', 'Canada']
+
+# List of available product database with price
+# Duplicates are intentional to simulate non-uniform data
+STOCK_DATA = [
+    {"Description": "WHITE HANGING HEART T-LIGHT HOLDER", "UnitPrice": 2.55, "StockCode": 3001},
+    {"Description": "WHITE HANGING HEART T-LIGHT HOLDER", "UnitPrice": 2.55, "StockCode": 3001},
+    {"Description": "WHITE HANGING HEART T-LIGHT HOLDER", "UnitPrice": 2.55, "StockCode": 3001},
+    {"Description": "WHITE METAL LANTERN", "UnitPrice": 3.39, "StockCode": 3002},
+    {"Description": "WHITE METAL LANTERN", "UnitPrice": 3.39, "StockCode": 3002},
+    {"Description": "WHITE METAL LANTERN", "UnitPrice": 3.39, "StockCode": 3002},
+    {"Description": "CREAM CUPID HEARTS COAT HANGER", "UnitPrice": 2.75, "StockCode": 3003},
+    {"Description": "CREAM CUPID HEARTS COAT HANGER", "UnitPrice": 2.75, "StockCode": 3003},
+    {"Description": "KNITTED UNION FLAG HOT WATER BOTTLE", "UnitPrice": 3.39, "StockCode": 3004},
+    {"Description": "KNITTED UNION FLAG HOT WATER BOTTLE", "UnitPrice": 3.39, "StockCode": 3004},
+    {"Description": "RED WOOLLY HOTTIE WHITE HEART.", "UnitPrice": 3.39, "StockCode": 3005},
+    {"Description": "RED WOOLLY HOTTIE WHITE HEART.", "UnitPrice": 3.39, "StockCode": 3005},
+    {"Description": "SET 7 BABUSHKA NESTING BOXES", "UnitPrice": 7.65, "StockCode": 3006},
+    {"Description": "GLASS STAR FROSTED T-LIGHT HOLDER", "UnitPrice": 4.25, "StockCode": 3007},
+    {"Description": "HAND WARMER UNION JACK", "UnitPrice": 1.85, "StockCode": 3008},
+    {"Description": "HAND WARMER RED POLKA DOT", "UnitPrice": 1.85, "StockCode": 3009},
+    {"Description": "ASSORTED COLOUR BIRD ORNAMENT", "UnitPrice": 1.69, "StockCode": 3010},
+    {"Description": "ASSORTED COLOUR BIRD ORNAMENT", "UnitPrice": 1.69, "StockCode": 3010},
+    {"Description": "ASSORTED COLOUR BIRD ORNAMENT", "UnitPrice": 1.69, "StockCode": 3010},
+    {"Description": "ASSORTED COLOUR BIRD ORNAMENT", "UnitPrice": 1.69, "StockCode": 3010},
+]
+
+
+def write_orders():
+    """
+    Generate orders per second based on predefined products
+
+    Returns:
+        None
+    """
+    kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
+    print('Writing records into Kafka. Kafka Server - {}, Topic - {}'.format(
+        ','.join(KAFKA_BOOTSTRAP_SERVER),
+        KAFKA_TOPIC))
+
+    invoice_no = START_INVOICE_ID_FROM
+
+    while True:
+
+        # To generate orders or not for this second
+        if random.choice([True, False]):
+
+            # Pick no. of orders to generate per sec
+            for _ in range(
+                    random.randint(MIN_ORDERS_PER_SEC, MAX_ORDERS_PER_SEC)):  # Orders in a sec
+
+                invoice_no += 1
+                invoice_date = int(datetime.datetime.now().timestamp())
+                country = random.choice(COUNTRIES)
+                customer_id = random.randint(MIN_CUSTOMER_ID, MAX_CUSTOMER_ID)
+
+                # No. of products to include in the order
+                n_products = random.randint(MIN_PRODUCTS_PER_ORDER, MAX_PRODUCTS_PER_ORDER)
+
+                # Pick n_products randomly from STOCK_DATA
+                for product in random.sample(STOCK_DATA, n_products):  # Product Selection
+                    order = {
+                        "InvoiceNo": invoice_no,
+                        "InvoiceDate": invoice_date,
+                        "CustomerID": customer_id,
+                        "Country": country,
+                        "StockCode": product['StockCode'],
+                        "Description": product['Description'],
+                        "Quantity": random.randint(MIN_PRODUCTS_PER_ORDER, MAX_PRODUCTS_PER_ORDER),
+                        "UnitPrice": product['UnitPrice'],
+                    }
+                    kafka_producer.send(KAFKA_TOPIC, str.encode(json.dumps(order)))
+                # End Product Selection for loop
+
+                if invoice_no % 100 == 0:
+                    print('{} records are written'.format(invoice_no))
+
+            # Ends Orders in a sec for loop
+
+        time.sleep(1)
+
+
+def main():
+    write_orders()
+
+
+if __name__ == '__main__':
+    main()
