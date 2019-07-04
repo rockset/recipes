8f30ddc04f75592436be325a821496374c2da356
Rui Aguiar <raguiar@rockset.com>
2019-07-04 00:04:23 +0000

    added failover and batching

--- rockkube/watcher.py
+++ rockkube/watcher.py
@@ -10,48 +10,72 @@ rs = rockset.Client(api_key=ROCKSET_API_KEY)
 
 # Fetch collection called kubernetes_events. If it does not exist, create it
 try:
-    events = rs.Collection.retrieve(COLLECTION_NAME)
+    collection = rs.Collection.retrieve(COLLECTION_NAME)
 except rockset.exception.InputError as e:
-    events = rs.Collection.create(COLLECTION_NAME)
+    collection = rs.Collection.create(COLLECTION_NAME)
 
 # Configs can be set in Configuration class directly or using helper utility
+# Here, we are loading it from the default location
 config.load_kube_config()
 
-# load kubernetes client
+# create kubernetes API instance
 v1 = client.CoreV1Api()
 v1ext = client.ExtensionsV1beta1Api()
+
+
+def generate_event_info(event, watch):
+    # The events are structured slightly differently between 
+    # batch ingest and watcher streams, so we 
+    # have two cases for generating the event_info document
+    if watch:
+        event_info = event['object'].to_dict()["metadata"]
+        event_info['resource_type'] = event['object'].kind
+        event_info['event_type'] = event['type']
+    else:
+        event_info = event.to_dict()['metadata']
+        event_info['resource_type'] = event.involved_object.kind
+        event_info['event_type'] = event.type
+    return event_info
+
+
+# Because we get a large number of events at the start of watching 
+# we batch these into several groups with max size limit_size
+def batch_upload_events():
+    resource_version = 0
+    while True:
+        limit_size = 500
+        events = v1.list_event_for_all_namespaces(limit=limit_size)
+        continue_tok = events.metadata._continue
+        resource_version = events.metadata.resource_version
+        event_docs = []
+        for event in events.items:
+            event_docs.append(generate_event_info(event, False))
+        collection.add_docs(event_docs)
+        if not continue_tok:
+            break
+    return resource_version
 
 # Create a watcher around the stream, and then
 # for each event, process the data from the event
 # and send to rockset as part of the kubernetes_events collection
-def watch_resource(resource):
+def watch_events():
     w = watch.Watch()
-    for event in w.stream(resource):
-        event_info = event['object'].to_dict()["metadata"]
-        event_info['resource_type'] = event['object'].kind
-        event_info['event_type'] = event['type']
-        events.add_docs([event_info])
+    resource_version = batch_upload_events()
+    doc_buffer = []
+    for event in w.stream(v1.list_event_for_all_namespaces, _request_timeout=60, resource_version=resource_version):
+        event_info = generate_event_info(event, True)
+        doc_buffer.append(event_info)
+        # try to add all pending docs to the rockset collection
+        # if we are successful, we can flush the buffer
+        try:
+            rs_info = collection.add_docs(doc_buffer)
+            doc_buffer = []
+        # For some reason, the rockset client failed, so we 
+        # keep the doc in the buffer and try again on the next event
+        except Exception as e:
+            # If we fail 25 times, assume there is some kind of 
+            # external issue and stop the watcher.
+            if len(doc_buffer) > 25:
+                break
 
-# We want to watch many different types of resources
-# so we spawn a thread with a watcher for each type we
-# are interested in
-resources = [
-    v1.list_pod_for_all_namespaces,
-    v1.list_config_map_for_all_namespaces,
-    v1ext.list_deployment_for_all_namespaces,
-    v1ext.list_ingress_for_all_namespaces,
-    v1ext.list_daemon_set_for_all_namespaces,
-    v1ext.list_network_policy_for_all_namespaces,
-    v1ext.list_replica_set_for_all_namespaces,
-]
-threads = []
-# Spawn a thread for each resource we are watching
-# so we can process events in parallel
-for (idx, resource) in enumerate(resources):
-    thread = threading.Thread(target=watch_resource, name='t'+str(idx), args=(resource,))
-    threads.append(thread)
-
-# We want the watchers to continue indefinitley, so we start them and run
-# them forever
-for thread in threads:
-    thread.start()
+watch_events()
