e86cd1030e51977406adb7d88da00b5cce2f01e5
Rui Aguiar <raguiar@rockset.com>
2019-07-04 01:11:22 +0000

    saved entire event info

--- rockkube/watcher.py
+++ rockkube/watcher.py
@@ -2,8 +2,9 @@ from kubernetes import client, config, watch
 import threading
 import rockset
 import os
+import argparse
 
-# Before watching events, connect to the rockset client
+# Before watching events, define constants and connect to the rockset client
 ROCKSET_API_KEY = os.environ['ROCKSET_API_KEY'] # replace with your API key here
 COLLECTION_NAME = 'kubernetes_events'
 rs = rockset.Client(api_key=ROCKSET_API_KEY)
@@ -28,23 +29,23 @@ def generate_event_info(event, watch):
     # batch ingest and watcher streams, so we 
     # have two cases for generating the event_info document
     if watch:
-        event_info = event['object'].to_dict()["metadata"]
-        event_info['resource_type'] = event['object'].kind
-        event_info['event_type'] = event['type']
+        event_info = event
     else:
-        event_info = event.to_dict()['metadata']
-        event_info['resource_type'] = event.involved_object.kind
-        event_info['event_type'] = event.type
+        event_info = event.to_dict()
     return event_info
 
 
 # Because we get a large number of events at the start of watching 
-# we batch these into several groups with max size limit_size
+# We batch these into several groups with max size limit_size
+# This function returns the resource version, which is the number before which
+# our watcher does not return events. This prevents us from getting 
+# duplicates when we stream the events for all namespaces
 def batch_upload_events():
     resource_version = 0
+    continue_tok = ''
     while True:
         limit_size = 500
-        events = v1.list_event_for_all_namespaces(limit=limit_size)
+        events = v1.list_event_for_all_namespaces(limit=limit_size, _continue = continue_tok)
         continue_tok = events.metadata._continue
         resource_version = events.metadata.resource_version
         event_docs = []
@@ -58,24 +59,36 @@ def batch_upload_events():
 # Create a watcher around the stream, and then
 # for each event, process the data from the event
 # and send to rockset as part of the kubernetes_events collection
-def watch_events():
+def watch_events(max_failures):
     w = watch.Watch()
     resource_version = batch_upload_events()
     doc_buffer = []
-    for event in w.stream(v1.list_event_for_all_namespaces, _request_timeout=60, resource_version=resource_version):
+    consecutive_failures = 0
+    # Here, resource version allows us to not fetch a large batch of events
+    for event in w.stream(v1.list_event_for_all_namespaces, resource_version=resource_version):
         event_info = generate_event_info(event, True)
         doc_buffer.append(event_info)
         # try to add all pending docs to the rockset collection
-        # if we are successful, we can flush the buffer
+        # if we are successful, we can flush the buffer and reset fail count
         try:
+            # TODO: save resource version here so we don't duplicate updates
             rs_info = collection.add_docs(doc_buffer)
             doc_buffer = []
+            consecutive_failures = 0
         # For some reason, the rockset client failed, so we 
         # keep the doc in the buffer and try again on the next event
         except Exception as e:
-            # If we fail 25 times, assume there is some kind of 
+            consecutive_failures += 1
+            print("Failed uploading to Rockset {} consecutive times".format(consecutive_failures, max_failures))
+            # If we fail MAX_FAILURES times, assume there is some kind of 
             # external issue and stop the watcher.
-            if len(doc_buffer) > 25:
+            if len(doc_buffer) > max_failures:
+                print("ERROR: Failed to upload to rockset {} times in a row. Terminating watcher.".format(max_failures))
                 break
 
-watch_events()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("max_failures", nargs='?', default=25)
+    args = parser.parse_args()
+    watch_events(args.max_failures)
